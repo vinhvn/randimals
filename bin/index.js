51d74c597426c3b7233152013b089f819b8cd873
vinhvn <vincentn337@gmail.com>
2021-06-05 15:10:17 -0400

    feat: added cli tool

---
+++ bin/index.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+const command = require('commander')
+const randimals = require('../dist/index.js');
+
+command
+  .version(randimals.version)
+  .option('-a, --adjectives <number>', 'set the number of adjectives to output', 1)
+  .option('-n, --animals <number>', 'set the number of animals to output', 1)
+  .option('-s, --separator <string>', 'set the separator string between each adjective and noun (defaults to space)', ' ')
+  .parse(process.argv);
+
+const options = command.opts();
+
+console.log(randimals({
+  adjectives: options.adjectives,
+  animals: options.animals,
+  separator: options.separator
+}));
