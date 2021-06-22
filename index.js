ed85ea7f5b910e84e3df7ef6c4a6dd3100a6beec
vinhvn <vincentn337@gmail.com>
2021-06-22 13:33:03 -0400

    chore: fixed default import and main on package.json

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * Randimals <https://github.com/vinhvn/randimals>
+ *
+ * Copyright © 2021, Vincent Nguyen.
+ * Released under the MIT License.
+ */
+'use strict';
+
+module.exports = require('./dist/index');
